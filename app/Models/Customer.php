2b2a81ce34101674355d3a74166c96030416694b
Fabio Schunig <fabio.schunig@gmail.com>
2025-10-24 20:21:17 -0300

    feat: implement Customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Customer extends Model
 {
-    /** @use HasFactory<\Database\Factories\CustomerFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'document',
+        'email',
+        'phone',
+        'address',
+    ];
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
 }
