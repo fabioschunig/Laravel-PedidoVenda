f1c17e3cf756f3a0747bbe4f95de1f7d4d91a872
Fabio Schunig <fabio.schunig@gmail.com>
2025-10-30 16:57:17 -0300

    feat: implement OrderFactory

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -2,7 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Enums\OrderStatus;
+use App\Models\Customer;
 use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,7 +22,22 @@ class OrderFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'customer_id' => Customer::factory(),
+            'status' => fake()->randomElement(OrderStatus::cases()),
+            'total' => 0, // será recalculado após criar os itens
         ];
+    }
+
+    public function configure(): static
+    {
+        return $this->afterCreating(function (Order $order) {
+            OrderItem::factory()
+                ->count(fake()->numberBetween(1, 5))
+                ->for($order)
+                ->create();
+
+            $order->recalculateTotal();
+        });
     }
 }
