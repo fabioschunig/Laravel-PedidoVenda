e1fdca8b671ece82b9fa38d99b4e9540043ad811
Fabio Schunig <fabio.schunig@gmail.com>
2025-11-12 19:15:17 -0300

    feat: add routes for managing customers with Livewire components

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Livewire\Customers\Index as CustomersIndex;
+use App\Livewire\Customers\Create as CustomersCreate;
+use App\Livewire\Customers\Edit as CustomersEdit;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -15,6 +19,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/clientes', CustomersIndex::class)->name('customers.index');
+    Route::get('/clientes/criar', CustomersCreate::class)
+        ->name('customers.create')
+        ->middleware('can:manage-customers');
+    Route::get('/clientes/{customer}/editar', CustomersEdit::class)
+        ->name('customers.edit')
+        ->middleware('can:manage-customers');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
