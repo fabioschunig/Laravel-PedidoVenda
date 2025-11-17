a87dc696bfe2fe7452605cbc56f00e4a27ffa000
Fabio Schunig <fabio.schunig@gmail.com>
2025-11-17 18:31:17 -0300

    feat: add Livewire component and view for order editing

---
+++ app/Livewire/Orders/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Orders;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.orders.edit');
+    }
+}
